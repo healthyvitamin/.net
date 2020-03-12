fc2fa51484b5bddef0049b55cadf44242298183b
L-carnitine <39097885+healthyvitamin@users.noreply.github.com>
2020-03-12 20:21:53 +0800

    Add files via upload

---
+++ 第二週上課_3_12/Enum_test.vb
@@ -0,0 +1,18 @@
+﻿Module Enum_test
+    Enum test As Integer
+        VB = 1
+        CSharp = 2
+        CPlusDouble = 3
+        JSharp = 4
+
+    End Enum
+
+    Sub Main()
+        Console.WriteLine("VB =" & test.VB)
+        Console.WriteLine("C# =" & test.CSharp)
+        Console.WriteLine("C++ =" & test.CPlusDouble)
+        Console.WriteLine("J# =" & test.JSharp)
+        Console.Read()
+    End Sub
+
+End Module
---
+++ 第二週上課_3_12/circle.vb
@@ -0,0 +1,17 @@
+﻿Module circle
+
+    Sub Main()
+        Const PI As Double = 3.14159
+        Console.WriteLine("請輸入半徑")
+        Dim radius As Double = Console.ReadLine()
+        Dim circumference As Double = radius * 2 * PI
+        Dim area As Double = radius * radius * PI
+
+        Console.Write("圓周:" & circumference)
+        Console.Write("面積:" & area)
+        Console.Read()
+
+
+    End Sub
+
+End Module
